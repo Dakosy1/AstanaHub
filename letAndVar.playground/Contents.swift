9cc90a7f7a53a3bb0840f2eba636d09e0d238a51
Dakosy1 <115999372+Dakosy1@users.noreply.github.com>
2024-03-21 23:35:48 +0500

    Update Contents.swift

--- letAndVar.playground/Contents.swift
+++ letAndVar.playground/Contents.swift
@@ -1,7 +1,15 @@
 import UIKit
 
+//Task1
 let firstNumber = 100
 let secondNumber = 100
 
 var sumOfTwoNumbers = firstNumber + secondNumber
+print(firstNumber)
+print(secondNumber)
 print(sumOfTwoNumbers)
+
+
+//Task2
+let footballerName = "Cristiano Ronaldo"
+print(footballerName)
